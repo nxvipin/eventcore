d0d604283ee509b490b665ff3f9f757118393d61
Vipin Nair <swvist@gmail.com>
2013-04-30 05:26:55 +0530

    Adding helper functions to get user name/process

--- src/presence.erl
+++ src/presence.erl
@@ -1,6 +1,10 @@
 -module(presence).
 
--export([create/2, delete/1]).
+-export([create/2,
+		 delete/1,
+		 get_username/1,
+		 get_userprocess/1
+		]).
 
 -define(RPOOL, {global, redispool}).
 
@@ -14,3 +18,9 @@ delete(UserProcess) ->
 	redis_pool:del(?RPOOL, Del),
 	redis_pool:del(?RPOOL, "presence:" ++ Username),
 	ok.
+
+get_username(UserProcess) ->
+	redis_pool:get(?RPOOL, "process:"++UserProcess).
+
+get_userprocess(Username) ->
+	redis_pool:get(?RPOOL, "presence:"++Username).
